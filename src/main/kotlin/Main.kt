66a69fe71159544ae43205796b0d920b624e0e35
Angiefiona <angellamumoh@gmail.com>
2023-03-09 08:56:26 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main(){
+    var txt = word("Congratulations", "Right")
+    println(txt)
+    numArrays()
+     name()
+    value()
+
+
+}
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+
+fun word(name:String, name2:String):String{
+var txt = ("Congratulation")
+    return(txt)
+
+}
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+fun numArrays(num1: Int, num2:Int, num3:Int, num4:Int){
+var numbers = arrayOf(67, 45, 10, 3)
+    println(num3)
+
+}
+//Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+fun name(){
+    var name1 = ("Teresiah")
+    println(name1[0]+" "+ name1[1]+ " "+name1[2]+" "+name1[3]+ " "+name1[4]+ " "+ name1[5]+ " "+name1[6]+ " "+name1[7],)
+}
+// Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+fun value(){
+    var numArray = arrayOf(64, 75, 20)
+    println(numArray.contentToString())
+}
+//Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
